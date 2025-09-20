7357d7953de3d1d6f36d1a25550bf26909459305
tapos-chandro <taposchandro231@gmail.com>
2025-09-20 19:56:55 +0600

    -App Structure -Navigation -Layouts

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <ul className='flex gap-4 justify-center p-4 '>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default NavBar;
