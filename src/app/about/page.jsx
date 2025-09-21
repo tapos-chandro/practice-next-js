194dfc25ffd4811b86b5362e36efc652a29c955f
tapos-chandro <taposchandro231@gmail.com>
2025-09-21 22:48:56 +0600

    about page seo has been done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
 import abutImage from "@/../public/using-laptop.webp"
 
+
+export const metadata = {
+  title: "About Page",
+  description: "Learn more about our company and team.",
+};
+
+
 export default function AboutPage() {
   return (
     <div>
