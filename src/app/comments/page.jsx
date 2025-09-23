62a1ef16d9f599ec227109aa53321591ab6a12fc
tapos-chandro <taposchandro231@gmail.com>
2025-09-23 17:06:45 +0600

    edit comments page

--- src/app/comments/page.jsx
+++ src/app/comments/page.jsx
@@ -1,10 +1,25 @@
-import React, { Suspense } from 'react'
-import CommentsList from '../components/CommentsList'
+"use client";
 
-export default function page() {
+
+import useSWR from "swr"
+
+const fetcher = url => fetch(url).then(r => r.json())
+
+export default function Comments() {
+
+  const { data, error, isLoading } = useSWR(`https://jsonplaceholder.typicode.com/comments`, fetcher, { suspense: true })
+
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <CommentsList />
-    </Suspense>
+    <div className="grid grid-cols-3 gap-4 p-5 max-w-7xl mx-auto">
+
+      {data.map(comment => (
+        <div className="p-4 rounded shadow-2xl" key={comment.id}>
+          <h3 className="text-md font-bold">{comment.name}</h3>
+          <p>{comment.body}</p>
+        </div>
+      ))}
+
+    </div>
   )
 }
